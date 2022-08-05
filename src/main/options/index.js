44e1abbb6e41f7edc92475012886f0a37d6ac800
Luis Float <contact@luisfloat.com>
2022-08-05 18:25:19 -0300

    feat(main): add motion options

--- src/main/options/index.js
+++ src/main/options/index.js
@@ -1,7 +1,9 @@
 import color from "./color.js";
 import font from "./font.js";
+import motion from "./motion.js";
 
 export default {
     color, 
     font,
+    motion,
 };
---
+++ src/main/options/motion.js
@@ -0,0 +1,19 @@
+export default {
+    "duration": {
+        "none": "0ms",
+        "xxs": "100ms",
+        "xs": "200ms",
+        "s": "400ms",
+        "m": "800ms",
+        "xm": "1200ms",
+        "l": "1600ms",
+        "xl": "2400ms",
+        "xxl": "3200ms",
+        "xxxl": "4800ms",
+        "j": "6400ms",
+    },
+    "ease": {
+        "1": "ease-out",
+        "2": "cubic-bezier(0.075, 0.82, 0.165, 1)",
+    },
+};
