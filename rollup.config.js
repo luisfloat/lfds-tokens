822318e4386eb36c770d32900f31e92d4f2d19cc
Luis Float <contact@luisfloat.com>
2022-08-05 18:00:57 -0300

    feat(build): add rollup as js bundler

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+    input: "./src/main/index.js",
+    output: {
+        file: "./dist/lfds-corallia.js",
+    },
+    plugins: [
+        nodeResolve(),
+        terser(),
+    ],
+};
