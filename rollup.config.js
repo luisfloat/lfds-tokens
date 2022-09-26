46040372271c334c4cd43cd6578f6d46816481b6
Luis Float <contact@luisfloat.com>
2022-09-25 23:46:14 -0300

    feat(build): add cjs support

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,24 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
+import fs from "fs";
+
+const pkg = JSON.parse(fs.readFileSync("package.json"));
 
 export default {
     input: "./src/main/core/index.js",
-    output: {
-        file: "./dist/lfds-tokens.js",
-    },
+    output: [
+        {
+            file: pkg.module,
+            format: "esm",
+            plugins: [ terser() ],
+        },
+        {
+            file: pkg.main,
+            format: "cjs",
+            exports: "auto",
+            plugins: [ terser() ],
+        },
+    ],
     plugins: [
         nodeResolve(),
         terser(),
